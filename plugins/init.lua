02c83ffd46a5ca220b4492b3da61660189f01d79
Dima Kachurovskyy <mekaa2.718@gmail.com>
2023-11-10 09:58:34 -0500

    setup color theme

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,4 +1,13 @@
-local default = {}
+local default = {
+  {
+    "lalitmee/cobalt2.nvim",
+    event = { "ColorSchemePre" },
+    dependencies = { "tjdevries/colorbuddy.nvim" },
+    init = function()
+        require("colorbuddy").colorscheme("cobalt2")
+    end,
+  },
+}
 
 local lazy = require("boot.lazy")
 
