1ccec6391208b31cf81c1bb9da91121ae9b63195
Dima Kachurovskyy <mekaa2.718@gmail.com>
2023-12-10 23:12:30 -0500

    init telescope; other navigation extensions

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,40 @@
+
+
 local plugins = {
+  "neovim/nvim-lspconfig",
+  "nvim-lua/plenary.nvim",
+  -- treesitter
   {
-    "neovim/nvim-lspconfig"
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        local tree_sitter = require("plugins.configs.treesitter")
+        require("nvim-treesitter.configs").setup(tree_sitter)
+    end
   },
   "alexaandru/nvim-lspupdate",
+
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
+    cmd = "Telescope",
+    opts = function()
+      return require "plugins.configs.telescope"
+    end,
+    config = function(_, opts)
+      local telescope = require "telescope"
+      telescope.setup(opts)
+
+      -- load extensions
+      for _, ext in ipairs(opts.extensions_list) do
+        telescope.load_extension(ext)
+      end
+    end,
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+  },
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
@@ -10,6 +42,8 @@ local plugins = {
   },
 }
 
+-- check if lazyvim installed
 local lazy = require("boot.lazy")
-
 lazy.setup(plugins)
+
+require("harpoon").setup()
