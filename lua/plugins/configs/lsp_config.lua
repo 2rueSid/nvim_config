bd1579ad0bf4db3359bd29821a2f05983c169af5
Dima Kachurovskyy <mekaa2.718@gmail.com>
2023-12-18 11:15:07 -0500

    add support for python to check poetry venv in the working dir

--- lua/plugins/configs/lsp_config.lua
+++ lua/plugins/configs/lsp_config.lua
@@ -1,3 +1,6 @@
+local util = require "lspconfig/util"
+local path = util.path
+ 
 local function lspSymbol(name, icon)
   local hl = "DiagnosticSign" .. name
   vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
@@ -128,10 +131,29 @@ lspconfig.lua_ls.setup {
   },
 }
 
+local function get_python_path(workspace)
+    -- Use activated virtualenv.
+    if vim.env.VIRTUAL_ENV then
+        return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+    end
+ 
+    -- Find and use virtualenv via poetry in workspace directory.
+    local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
+    if match ~= "" then
+        local venv = vim.fn.trim(vim.fn.system "poetry env info -p")
+        return path.join(venv, "bin", "python")
+    end
+ 
+    -- Fallback to system Python.
+    return vim.fn.exepath "python3" or vim.fn.exepath "python" or "python"
+end
+ 
 lspconfig.pyright.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
+  on_attach=on_attach,
+  capabilities=capabilities,
+  on_init = function(client)
+        client.config.settings.python.pythonPath = get_python_path(client.config.root_dir)
+  end,
 }
-
 
 return {capabilities=capabilities, on_attach=on_attach }
