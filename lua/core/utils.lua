5e3270b2802a5aa20a7d522cdbbd6199bffcee5e
Dima Kachurovskyy <mekaa2.718@gmail.com>
2024-02-05 21:30:22 -0500

    updated core files

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,9 +1,36 @@
 local function open_file_by_path(path)
-    if path ~= "" then
-        vim.cmd('edit ' .. path)
-    end
+	if path ~= "" then
+		vim.cmd("edit " .. path)
+	end
+end
+
+function show_documentation()
+	if vim.bo.filetype == "help" then
+		vim.cmd([[h ]] .. vim.fn.expand("<cword>"))
+	else
+		vim.lsp.buf.hover()
+	end
+end
+
+function format_async(err, result, ctx)
+	if err ~= nil or result == nil then
+		return
+	end
+
+	if not vim.api.nvim_buf_get_option(ctx.bufnr, "modified") then
+		local view = vim.fn.winsaveview()
+		local client = vim.lsp.get_client_by_id(ctx.client_id)
+		vim.lsp.util.apply_text_edits(result, ctx.bufnr, client.offset_encoding)
+		vim.fn.winrestview(view)
+
+		if ctx.bufnr == vim.api.nvim_get_current_buf() then
+			vim.cmd([[noautocmd :update]])
+		end
+	end
 end
 
 return {
-  open_file = open_file_by_path
+	open_file = open_file_by_path,
+	format_async = format_async,
+	show_documentation = show_documentation,
 }
--- lua/core/vim.lua
+++ lua/core/vim.lua
@@ -32,16 +32,38 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
 -- use sys clipboard
 vim.opt.clipboard:append("unnamedplus")
 
--- cmp
-vim.opt.completeopt = { "menuone", "noselect", "noinsert" }
-vim.opt.shortmess = vim.opt.shortmess + { c = true }
-vim.api.nvim_set_option("updatetime", 300)
+vim.g.floating_window_border = {
+	"╭",
+	"─",
+	"╮",
+	"│",
+	"╯",
+	"─",
+	"╰",
+	"│",
+}
 
--- Fixed column for diagnostics to appear
--- Show autodiagnostic popup on cursor hover_range
--- Goto previous / next diagnostic warning / error
--- Show inlay_hints more frequently
-vim.cmd([[
-set signcolumn=yes
-autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
-]])
+local disabled_built_ins = {
+	"netrw",
+	"netrwPlugin",
+	"netrwSettings",
+	"netrwFileHandlers",
+	"gzip",
+	"zip",
+	"zipPlugin",
+	"tar",
+	"tarPlugin",
+	"getscript",
+	"getscriptPlugin",
+	"vimball",
+	"vimballPlugin",
+	"2html_plugin",
+	"logipat",
+	"rrhelper",
+	"spellfile_plugin",
+	"matchit",
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+	vim.g["loaded_" .. plugin] = 1
+end
